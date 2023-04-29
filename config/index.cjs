df3dcec117106158adc1e5be3b2a54159b063eee
pengcheng933 <104008568+pengcheng933@users.noreply.github.com>
2023-04-29 12:46:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'a76f7ea25e7ab447988850e589a186b3',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'yHHlghyciX78JGS_PDwRm8WgI93bFpgDYeOrV2IJps4',
+      useTemplateId: 'WNeXVs_YMlyM78Hb6KPhIQyJtFxpomVGla8GGtrNWkU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-01',
       festivals: [
