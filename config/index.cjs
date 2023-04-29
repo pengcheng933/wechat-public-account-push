d78af0f84b08e73bdde58350e935f39dafec3364
pengcheng933 <104008568+pengcheng933@users.noreply.github.com>
2023-04-29 13:02:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -55,9 +55,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '宝贝',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oqdX65yUPJopppu_YvHHlVGATTqY',
+      id: 'oqdX65_RM-aEy2h15UQgH7vbIGN8',
     }
   ],
 
